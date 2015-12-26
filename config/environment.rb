beac09e05d3c0349b91ba537f28998fe44438317
Kenneth Dillon <kenn.dillon55@gmail.com>
2015-12-26 22:20:10 +0000

    Conflict fix attempt

--- config/environment.rb
+++ config/environment.rb
@@ -8,17 +8,7 @@ ActionMailer::Base.stmp_settings = {
     :address => 'stmp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-<<<<<<< HEAD
-<<<<<<< HEAD
-    :user_name => ENV['SENDGRID_USERNAME'],
-=======
-    :username => ENV['SENDGRID_USERNAME'],
->>>>>>> e8675bf0f1dbf1bcbff087fe1927c6a7cf84d09a
-=======
-    :username => ENV['SENDGRID_USERNAME'],
->>>>>>> e8675bf0f1dbf1bcbff087fe1927c6a7cf84d09a
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
-    :enable_startstls_auto => true
+
+
     
 }
