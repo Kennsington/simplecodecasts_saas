5c8cda00c8e779ca71edc162c6aee8a9ef5d7549
Kenneth Dillon <kenn.dillon55@gmail.com>
2016-01-06 20:43:53 +0000

    Added profiles_new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,8 @@
 class ProfilesController < ApplicationController
+    def new
+        #pseudo code: form where each user can fill out their own profile.
+        @user = User.find( params[:user_id] )# this params argument determines who is logged in
+        @profile = @user.build_profile
+    end
+    
 end
