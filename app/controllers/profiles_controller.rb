03d6c7b8112bc7baff0b0d122e456b85f8b51989
Kenneth Dillon <kenn.dillon55@gmail.com>
2016-01-07 00:32:00 +0000

    Added profiles form and create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,4 +5,20 @@ class ProfilesController < ApplicationController
         @profile = @user.build_profile
     end
     
+    def create
+        @user = User.find(params [:user_id])
+        @profile = @user.build_profile(profile_params)
+        if @profile.save
+            flash[:success] = "Your profile has been updated!"
+            redirect_to user_path(params[:user_id]) 
+        else
+            render action: :new #if error occurs renders new action above to restart update profile
+        end
+    end
+    
+    private
+    #Necessary to whiteist arguments that make up the profile paramaters
+        def profile_params
+            params.require(:profile).permit(:first_name, :last_name, :phone_number, :contact_email, :description)
+        end
 end
