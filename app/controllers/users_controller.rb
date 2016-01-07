64d934648ebbafc5bd7b9665071434fddafd34ad
Kenneth Dillon <kenn.dillon55@gmail.com>
2016-01-07 19:51:59 +0000

    Added bare bones user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
     def show
-       @user = User.find(params[:id]) #:id because this was indicated in the path in rake routes info. 
+       @user = User.find( params[:id]) #:id because this was indicated in the path in rake routes info. 
     end
 end
