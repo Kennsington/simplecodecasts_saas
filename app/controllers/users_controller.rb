e9e519a48c205497ed7d7aad691b642baa36f4a6
Kenneth Dillon <kenn.dillon55@gmail.com>
2016-01-07 22:46:04 +0000

    Added before filters to check correct user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+     before_action :authenticate_user! 
+        
     def show
        @user = User.find( params[:id]) #:id because this was indicated in the path in rake routes info. 
     end
