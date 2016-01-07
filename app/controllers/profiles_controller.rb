c93cab7452017f72b3438c387242ca533ac17fa5
Kenneth Dillon <kenn.dillon55@gmail.com>
2016-01-07 20:18:16 +0000

    Changed new action in profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
     def new
         #pseudo code: form where each user can fill out their own profile.
         @user = User.find( params[:user_id] )# this params argument determines who is logged in
-        @profile = @user.build_profile
+        @profile = Profile.new
     end
     
     def create
