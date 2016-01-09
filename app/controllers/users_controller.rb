e934879a8cb95db41860b588426176ad8f7d3a38
Kenneth Dillon <kenn.dillon55@gmail.com>
2016-01-09 18:52:21 +0000

    Added index action and updated link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
      before_action :authenticate_user! 
+     
+    def index
+        
+    end
         
     def show
        @user = User.find( params[:id]) #:id because this was indicated in the path in rake routes info. 
     end
+    
 end
